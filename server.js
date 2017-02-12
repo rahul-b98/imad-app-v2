a28f9cce672a57e8030fc95b2cf78dcc2f4991c6
rahul-b98 <rvrajamani@gmail.com>
2017-02-12 15:59:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,15 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
+app.get('/article-one',function(req,res){
+    res.send('article one will be served here');
+});
+app.get('/article-two',function(req,res){
+    res.send('article two will be served here');
+});
+app.get('/article-three',function(req,res){
+    res.send('article three will be served here');
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
